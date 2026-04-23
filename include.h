73a1a1054868fe2aa1731ffbce0e9f5f0e627296
uncle_joe <uncle_joe@kali.kali.com>
2026-04-23 10:35:12 +0300

    all definitions and include in one place

--- include.h
+++ include.h
@@ -1,11 +1,19 @@
-#pragma once
-#include"uthash.h"
+#ifndef INCLUDE_H
+#define INCLUDE_H
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <ctype.h>
+#include"uthash.h"
+
 struct Mapping {
     char ext[10];       // key
     char folder[20];    // value
     UT_hash_handle hh;
 };
+
+
 enum RETURN{
     SUCCESS = 0,
     FAIL = 1,
@@ -13,6 +21,10 @@ enum RETURN{
 
 };
 
-enum RETURN build_extension_folder_hashmap();
-char* get_folder(char* ext);
-void organize(char* path);
+extern struct Mapping defaults[];
+extern size_t defaults_size;
+
+extern enum RETURN build_extension_folder_hashmap();
+extern char* get_folder(char* ext);
+extern void organize(char* path);
+#endif
