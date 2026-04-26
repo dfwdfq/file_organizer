f3e08c28295d9a00323ce5d6b67ba3d46f0cc440
uncle_joe <uncle_joe@kali.kali.com>
2026-04-26 18:10:04 +0300

    use fprintf to print error messages in stderr

--- src/main.c
+++ src/main.c
@@ -5,21 +5,29 @@
 
 int main(int argc , char** argv) {
     if(argc == 1){
-        printf("Target Folder Path not Provided\n");
+      fprintf(stderr,"Target Folder Path not Provided\n");
         return EXIT_FAILURE;
     }
     if(argc != 2){
-        printf("Usage ./organize {Folder path to organize}\n");
+      fprintf(stderr,"Usage ./organize {Folder path to organize}\n");
         return EXIT_FAILURE;
     }
     char* path = argv[1];
     // build the extension to folder hashmap
-    enum RETURN ret = build_extension_folder_hashmap();
+    RETURN ret = build_extension_folder_hashmap();
     switch(ret){
-        case SUCCESS : printf("HashMap Build Success\n");break;
-        case FAIL : printf("Failure While building HashMap\n");return EXIT_FAILURE;
-        case SOMETHING_WRONG: printf("Something is wrong with hash format / config file\n");return EXIT_FAILURE;
-        default: printf("Unknown Return Code\n");return EXIT_FAILURE;
+    case SUCCESS :
+      fprintf(stderr,"HashMap Build Success\n");
+      break;
+    case FAIL:
+      fprintf(stderr,"Failure While building HashMap\n");
+      return EXIT_FAILURE;
+    case SOMETHING_WRONG:
+      fprintf(stderr,"Something is wrong with hash format / config file\n");
+      return EXIT_FAILURE;
+    default:
+      fprintf(stderr,"Unknown Return Code\n");
+      return EXIT_FAILURE;
     }
     // pick every file and move it into respective folders
     organize(path);
